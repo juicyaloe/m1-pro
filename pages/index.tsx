d29444007c0510216f729e40481e712486e6af25
Min-jae Kim <mink4661@snu.ac.kr>
2022-11-26 20:43:35 +0900

    메인 페이지 css 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,33 @@
+import styled from '@emotion/styled';
 import { Fragment, useEffect, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 
 import type { AccountData } from './api/accounts';
 
 import { Container } from '../components/ui';
+
+const AccountContainer = styled(Container)`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Currency = styled.div`
+  border-radius: 5px;
+  box-shadow: 3px 3px 5px #bebebe;
+
+  margin: 10px 0px;
+  padding: 5px 3px;
+`;
+
+const CaptionText = styled.span`
+  font-size: 17px;
+  font-weight: bold;
+  margin-right: 5px;
+`;
+
+const NormalText = styled.span`
+  font-size: 15px;
+`;
 
 async function get_accounts() {
   let response = await fetch('/api/accounts');
@@ -19,13 +43,26 @@ export default function Home() {
   if (isLoading) return <div>로딩중</div>;
 
   return (
-    <Container>
+    <AccountContainer>
       {data?.map(current => (
-        <div key={current.currency}>
-          <span>통화: {current.currency}/ </span>
-          <span>잔고: {parseFloat(current.balance)}</span>
-        </div>
+        <Currency
+          key={current.currency}
+          data-id={current.currency}
+          onMouseEnter={e => console.log(e)}
+          onMouseLeave={(e: any) => console.log(e.target.dataset.id)}
+        >
+          <CaptionText>{current.currency}: </CaptionText>
+          <NormalText>
+            {parseInt(current.balance)}{' '}
+            {current.currency != 'KRW'
+              ? `(원화 환산: ${
+                  parseFloat(current.balance) *
+                  parseFloat(current.avg_buy_price)
+                }원)`
+              : ''}
+          </NormalText>
+        </Currency>
       ))}
-    </Container>
+    </AccountContainer>
   );
 }
